bd71057b83d9834a49cabfb8a8b47221c1eefb12
Circle-A <yuval@circle-a-capital.com>
2024-04-14 16:52:12 +0300

    socket io

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const cors = require("cors");
+const socketIO = require('socket.io');
 
 const app = express();
 const port = 3000;
@@ -10,9 +11,25 @@ app.use(express.json());
 app.use(cors());
 
 
-
-
-
 const server = app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
+});
+
+
+
+const io = socketIO(server, {
+  cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+
+  socket.emit('initialFileChanges', fileChanges);
+
+  socket.on('disconnect', () => {
+      console.log('Client disconnected');
+  });
 });
