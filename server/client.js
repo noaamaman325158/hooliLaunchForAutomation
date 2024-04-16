6969cbc62f4ea73dce8e745a30bf0a0756ccd3c2
noaa <noaamaman325158@gmail.com>
2024-04-16 20:07:59 +0300

    basic connection between client and server in my local computer

---
+++ server/client.js
@@ -0,0 +1,32 @@
+const io = require('socket.io-client');
+const fs = require('fs');
+
+let serverIp = "192.168.1.116";
+const serverUrl = `http://${serverIp}:3000`; 
+
+console.log(`Connecting to server at: ${serverUrl}`);
+const socket = io(serverUrl);
+
+socket.on('connect', () => {
+    console.log('Connected to server');
+});
+
+socket.on('initialFileChanges', (fileChanges) => {
+    console.log('Initial file changes received:', fileChanges);
+});
+
+socket.on('fileChange', (path) => {
+    console.log('File change detected:', path);
+
+    fs.appendFile('local_file.txt', `${path}\n`, (err) => {
+        if (err) {
+            console.error('Error appending to local file:', err);
+            return;
+        }
+        console.log('Change appended to local file:', path);
+    });
+});
+
+socket.on('disconnect', () => {
+    console.log('Disconnected from server');
+});
--- server/server.js
+++ server/server.js
@@ -2,14 +2,26 @@
 const express = require('express');
 const cors = require("cors");
 const socketIO = require('socket.io');
+const socketIOClient = require('socket.io-client');
+
+const chokidar = require('chokidar');
 
 const app = express();
 const port = 3000;
 
-// Middleware to parse JSON bodies
 app.use(express.json());
 app.use(cors());
 
+
+const pathToWatch = "/home/noaa/Documents/NinjaTrader 8/outgoing/Globex_Source1_position.txt";
+const remotePathToWrite = "/home/noaa/Documents/NinjaTrader 8/outgoing/Globex_Source1_position.txt";
+let fileChangesTracking = [];
+let remoteComputerIp = "";
+let remotePort = ""
+const watcher = chokidar.watch(pathToWatch, {
+  ignored: /(^|[\/\\])\../, // ignore dotfiles
+  persistent: true
+});
 
 const server = app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
@@ -27,9 +39,28 @@ const io = socketIO(server, {
 io.on('connection', (socket) => {
   console.log('Client connected');
 
-  socket.emit('initialFileChanges', fileChanges);
+  socket.emit('initialFileChanges', fileChangesTracking);
 
   socket.on('disconnect', () => {
-      console.log('Client disconnected');
+
+    fs.appendFile(remotePathToWrite, `${path}\n`, (err) => {
+      if (err) {
+        console.error('Error appending to file:', err);
+        return;
+      }
+      console.log('Change appended to file:', path);
+    });
+    
   });
+  
+
+});
+
+watcher.on('change', (path) => {
+  fileChangesTracking.push(path);
+  const remoteSocket = socketIOClient(`http://${remoteComputerIp}:${remotePort}`);
+
+  remoteSocket.emit('The file is changed in the server side', path)
+
+  io.emit('fileChange', path);
 });
