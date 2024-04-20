54c01286c6e5f1b6ecfff06b916b46f25b1faaf1
noaa <noaamaman325158@gmail.com>
2024-04-20 23:36:53 +0300

    Add some additional endpoint of gfromet the allowed destinations from the settings.json file and fetch it from the UI of the client.

--- client_interface/src/MainClientInterface.jsx
+++ client_interface/src/MainClientInterface.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'; 
 import './mainClientInterface.css';
@@ -25,41 +24,13 @@ function ClientInterface() {
     };
 
     fetchAllowedDestinationsToTracking();
-    fetchClientCount(); // Fetch initial client count on mount
   }, []); 
-
-  const fetchClientCount = async () => {
-    try {
-      const response = await axios.get('http://localhost:3001/countConnectedClients');
-      setClientCount(response.data.count); // Assuming the endpoint returns an object with a count property
-    } catch (error) {
-      console.error('Failed to fetch client count:', error);
-    }
-  };
-
-  const handleConnect = () => {
-    setConnected(true);
-    fetchClientCount(); // Refresh the client count after connecting
-=======
-import React, { useState } from 'react';
-import './mainClientInterface.css';
-
-const accountsData = [
-  { id: 1, name: 'Account A' },
-  { id: 2, name: 'Account B' },
-  { id: 3, name: 'Account C' },
-];
-
-function ClientInterface() {
-  const [connected, setConnected] = useState(false);
-  const [selectedAccounts, setSelectedAccounts] = useState([]);
-  const [clientCount, setClientCount] = useState(0); // State to track the number of connected clients
 
   const handleConnect = () => {
     setConnected(true);
 >>>>>>> 3da80c1 (Add some static basic UI for the client)
     console.log('Connected to server!');
-    setClientCount(prevCount => prevCount + 1); // Increment client count upon connection
+    setClientCount(prevCount => prevCount + 1); 
   };
 
   const handleCheckboxChange = (accountId) => {
@@ -75,17 +46,7 @@ function ClientInterface() {
   return (
     <div className="table-container">
       <h1>Client Trade Copier Interface</h1>
-<<<<<<< HEAD
-<<<<<<< HEAD
       <div className="client-count">Current Connections: {clientCount}</div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Bag TO Track</th>
-=======
-=======
-      <div className="client-count">Current Connections: {clientCount}</div> {/* Display the number of connected clients */}
->>>>>>> 8457a3e (Add some segment to the UI that actually show the number of the connected clients in live.)
       <table className="table">
         <thead>
           <tr>
@@ -95,11 +56,7 @@ function ClientInterface() {
           </tr>
         </thead>
         <tbody>
-<<<<<<< HEAD
           {tableData.map(account => (
-=======
-          {accountsData.map(account => (
->>>>>>> 3da80c1 (Add some static basic UI for the client)
             <tr key={account.id}>
               <td>{account.name}</td>
               <td>
